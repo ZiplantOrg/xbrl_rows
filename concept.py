5008e90718221ba916244bf8efa05460ac822f7b
vladislav ziplant <v.sinitsyn@open-bs.ru>
2025-04-22 14:30:54 +0300

    работатает: XBRL_1107746281026_ep_nso_aif_uk_q_y_10rd_specdep_20250331; XBRL_1107746281026_ep_nso_purcb_m_10rd_reestr_0420417_20240131
    но не сразу для двух файлов

---
+++ concept.py
@@ -0,0 +1,4 @@
+data_cnc = {
+    "concept_1": './/{http://www.cbr.ru/xbrl/nso/purcb/dic/purcb-dic}*',
+    "concept_2": './/{http://www.cbr.ru/xbrl/nso/uk/dic}*'
+}
---
+++ context.py
@@ -0,0 +1,4 @@
+data_ctx = {
+    "ctx_1": './/{http://www.xbrl.org/2003/instance}scenario/{http://xbrl.org/2006/xbrldi}typedMember/{http://www.cbr.ru/xbrl/udr/dim/dim-int}ID_NomeraInformSoobshheniyaOSdelkeTypedName',
+    "ctx_2": './/{http://www.xbrl.org/2003/instance}scenario/{http://xbrl.org/2006/xbrldi}typedMember/{http://www.cbr.ru/xbrl/udr/dim/dim-int}ID_YULTypedName'
+}
